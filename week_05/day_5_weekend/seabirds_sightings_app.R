8e540606c5137cde42f017caecffcb777d6b6372
Thijmen18 <36414676+Thijmen18@users.noreply.github.com>
2023-06-24 15:55:23 +0100

    add leaflet map to app, and rearrange items

--- week_05/day_5_weekend/seabirds_sightings_app.R
+++ week_05/day_5_weekend/seabirds_sightings_app.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(plotly)
 library(leaflet)
 library(plotly)
+library(bslib)
 
 seabirds <- read_csv("data/seabirds_cleaned.csv")
 
@@ -21,12 +22,11 @@ rare_plot <- seabirds %>%
   aes(x = species_common, y = count_individual_birds) +
   geom_col(fill = "#e34a33") +
   coord_flip() +
-  ylab("Total individual birds observed") +
+  ylab("") +
   xlab("") +
-  ggtitle("Rarest birds observed") +
   theme_classic() +
   scale_y_continuous(breaks = 1:5) +
-  expand_limits(y = 5) 
+  expand_limits(y = 5)
 
 common_plot <- seabirds %>% 
   select(species_scientific, species_common, species_abbreviation, count) %>%
@@ -43,43 +43,93 @@ common_plot <- seabirds %>%
   aes(x = species_common, y = count_individual_birds) +
   geom_col(fill = "#addd8e") +
   coord_flip() +
-  ylab("Total individual birds observed") +
+  ylab("") +
   xlab("") +
-  ggtitle("Most common birds seen") +
   theme_classic() +
-  scale_y_continuous(labels = scales::comma) 
+  scale_y_continuous(labels = scales::comma)
+
+all_common <- 
+  seabirds %>% 
+  select(species_common) %>%
+  filter(!str_detect(species_common, "sensu lato")) %>% 
+  filter(!str_detect(species_common, "unidentified")) %>% 
+  distinct(species_common)
 
 ui <- fluidPage(
+  theme = bs_theme(bootswatch = "lux"),
+  
   fluidRow(
-    titlePanel("Seabird Observations New Zealand"),
-    h3("Key insights into bird obervations in the waters around New Zealand")
+    titlePanel(tags$b("Seabird Observations New Zealand"))
   ),
+  fluidRow(p("This app offers key insights on seabird observations in the waters around New Zealand")),
+  
   br(),br(),
+  fluidRow(
+    column(width = 11.5,
+           offset = 0.5,
+           titlePanel(tags$h4("Total bird species observed per year"))
+    )
+  ),
+  
   fluidRow(
     # I want to have a graph at the top of my dashboard showing a minimal representation
     # of total bird species observed in recent years. Gives a good overview of decline of obervations
     # over the years (biodiversity crisis or lack of observers?!)
-    plotOutput("plot_totalobserved")
+    column(width = 10, plotOutput("plot_totalobserved"))
   ),
+  
+  br(),br(),
   fluidRow(
-    column(width = 5,
-           offset = 7,
-           titlePanel(tags$h2("Total bird species observed in recent years"))
+    column(width = 11.5,
+           offset = 0.5,
+           titlePanel(tags$h4("Rarest and commonest seabirds in New Zealand"))
     )
   ),
   fluidRow(
-    column(width = 3,
+    column(width = 2.5,
+           offset = 1,
            radioButtons("scarcity_input",
-                        "Most common or rare birds in New Zealand",
-                        choices = c("Rarest", "Commonest"), # unfortunately, John's method does not work (since both arrange() and plot type has be changed!)
-                        inline = TRUE)
-    ),
+                        "",
+                        choices = c("Commonest", "Rarest"), # unfortunately, John's method does not work (since both arrange() and plot type has be changed!)
+                        inline = TRUE)),
+  ),
+  fluidRow(
+    
     column(width = 9,
+           offset = 1,
            # This plot shows the top 7 most rare or common birds seen based on the viewers choice. 
            # By hoovering over the bars, the viewers can see the exact number of individuals recorded in total.
-           plotOutput("scarcity_plot"))
+           plotlyOutput("scarcity_plot"))
+  ),
+  br(),
+  fluidRow(
+    column(width = 5,
+           offset = 0.5,
+           titlePanel(tags$h4("bird sighting locations"))
+    )
+  ),
+  fluidRow(
+    column(width = 4,
+           offset = 2.5,
+           selectInput(inputId = "species_input",
+                       label = "Select bird species",
+                       choices = all_common,
+                       selected = "Royal albatros"),
+    ),
+    fluidRow(
+    column(offset = 1,
+      width = 9,
+           #  # This map shows the locations of observations for the species of choice!
+           leafletOutput("leaflet_map"))
+    
+  ),
+  fluidRow(
+    column(width = 2,
+           offset = 10,
+           titlePanel(tags$h6("This app is created by Thijmen Breeschoten"))
   )
-  
+  )
+)
 )
 
 server <- function(input, output, session) {
@@ -104,7 +154,7 @@ server <- function(input, output, session) {
       aes(x = year, y = total_species_per_year) +
       geom_line(colour = "#08519c") +
       geom_area(fill = "#9ecae1") +
-      geom_point(colour = "#08519c", size = 4) +
+      geom_point(colour = "#08519c", size = 2) +
       geom_text(aes(label = label_text), hjust = -0.5, size = 14) +
       theme_void() +
       expand_limits(x = 1991)
@@ -117,10 +167,18 @@ server <- function(input, output, session) {
       common_plot
     }
   })
+  # This plot shows the top 7 most rare or common birds seen based on the viewers choice. 
+  # By hoovering over the bars, the viewers can see the exact number of individuals recorded in total.
+  output$scarcity_plot <- renderPlotly({plot()})
   
-  output$scarcity_plot <- renderPlot({plot()})
-  
-
+  # This map shows the locations of observations for the species of choice!
+  output$leaflet_map <- renderLeaflet({
+    seabirds %>% 
+      filter(str_detect(species_common, input$species_input)) %>% 
+      leaflet() %>% 
+      addProviderTiles(provider = providers$OpenStreetMap) %>% 
+      addMarkers(lng = ~long, lat = ~lat, popup = ~species_scientific)
+  })
 
 }
 
