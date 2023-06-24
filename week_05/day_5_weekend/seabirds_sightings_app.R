9948b64cd2d85e513260f67e41140f5c87cac032
Thijmen18 <36414676+Thijmen18@users.noreply.github.com>
2023-06-24 13:29:16 +0100

    Update seabirds_sightings_app.R

--- week_05/day_5_weekend/seabirds_sightings_app.R
+++ week_05/day_5_weekend/seabirds_sightings_app.R
@@ -5,26 +5,6 @@ library(leaflet)
 
 seabirds <- read_csv("data/seabirds_cleaned.csv")
 
-plot_totalobserved <- seabirds %>% 
-  mutate(year = lubridate::year(date)) %>% 
-  select(species_scientific, species_common, species_abbreviation, year) %>% 
-  filter(!str_detect(species_common, "sensu lato")) %>% 
-  filter(str_detect(species_scientific, "[A-Z]{1}[a-z]* [a-z]*$")) %>% 
-  filter(!str_detect(species_common, "unidentified")) %>% 
-  filter(!str_detect(species_scientific, "\\/")) %>% 
-  select(year, species_scientific) %>% 
-  group_by(year) %>% 
-  summarise(total_species_per_year = n()) %>% 
-  mutate(label_text = ifelse(year == 1990, total_species_per_year, NA)) %>% 
-  ggplot() +
-  aes(x = year, y = total_species_per_year) +
-  geom_line(colour = "#08519c") +
-  geom_area(fill = "#9ecae1") +
-  geom_point(colour = "#08519c", size = 4) +
-  geom_text(aes(label = label_text), hjust = -0.5, size = 8) +
-  theme_void() +
-  expand_limits(x = 1991)
-
 ui <- fluidPage(
   fluidRow(
     titlePanel(tags$h1("Seabird Observations New Zealand")),
@@ -33,7 +13,10 @@ ui <- fluidPage(
     titlePanel(tags$h3("Key insights into bird obervations in the waters around New Zealand"))
   ),
   fluidRow(
-    plotOutput(plot_totalobserved)
+    # I want to have a graph at the top of my dashboard showing a minimal representation
+    # of total bird species observed in recent years. Gives a good overview of decline of obervations
+    # over the years (biodiversity crisis or lack of observers?!)
+    plotOutput("plot_totalobserved")
     
   ),
   fluidRow(),
@@ -42,6 +25,32 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  
+  # I want to have a graph at the top of my dashboard showing a minimal representation
+  # of total bird species observed in recent years. Gives a good overview of decline of obervations
+  # over the years (biodiversity crisis or lack of observers?!)
+  
+  Output$plot_totalobserved <- renderPlot({
+    plot_totalobserved <- seabirds %>% 
+      mutate(year = lubridate::year(date)) %>% 
+      select(species_scientific, species_common, species_abbreviation, year) %>% 
+      filter(!str_detect(species_common, "sensu lato")) %>% 
+      filter(str_detect(species_scientific, "[A-Z]{1}[a-z]* [a-z]*$")) %>% 
+      filter(!str_detect(species_common, "unidentified")) %>% 
+      filter(!str_detect(species_scientific, "\\/")) %>% 
+      select(year, species_scientific) %>% 
+      group_by(year) %>% 
+      summarise(total_species_per_year = n()) %>% 
+      mutate(label_text = ifelse(year == 1990, total_species_per_year, NA)) %>% 
+      ggplot() +
+      aes(x = year, y = total_species_per_year) +
+      geom_line(colour = "#08519c") +
+      geom_area(fill = "#9ecae1") +
+      geom_point(colour = "#08519c", size = 4) +
+      geom_text(aes(label = label_text), hjust = -0.5, size = 8) +
+      theme_void() +
+      expand_limits(x = 1991)
+  })
   
   
   
